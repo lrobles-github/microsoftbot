1dbe110692216620db4cd5ed118bce8c0049660f
Luis Robles <luis.g.robles@outlook.com>
2018-02-17 13:20:07 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,130 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+
+var inMemoryStorage = new builder.MemoryBotStorage();
+
+var tasks = [];
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+    session.send("Welcome to the Tasks chatbot!")
+    session.beginDialog('askForUserName');
+    },
+    function (session, results) {
+        session.beginDialog('askForTask');
+    } 
+]).set('storage', inMemoryStorage);
+
+
+bot.dialog('askForUserName', [
+    function(session) {
+        builder.Prompts.text(session, 'What\'s your name?');
+    },
+    function (session, results) {
+        session.userData.userName = results.response;
+        session.endDialog(`Alright, ${results.response}!, I can create a task for you.`);
+    }
+]);
+
+bot.dialog('askForTask', [
+    function(session) {       
+        builder.Prompts.text(session, 'What is the task you want me to create?');
+    },
+    function (session, results) {
+        session.userData.tasks = results.response;
+        tasks.push(session.userData.tasks);
+        session.send(`Saved ${tasks[tasks.length - 1]}`);   
+        session.Prompts.time(session, 'At what date and time?')
+    },
+    function (session, results) {
+        session.dialogData.time = builder.EntityRecognizer.resolveTime([results.response]);
+        //session.send(session.userData.time + results.response);
+        session.endDialog('Bye!');
+    }
+]);
+
+
+
+// /*-----------------------------------------------------------------------------
+// A simple echo bot for the Microsoft Bot Framework. 
+// -----------------------------------------------------------------------------*/
+
+// var restify = require('restify');
+// var builder = require('botbuilder');
+// var botbuilder_azure = require("botbuilder-azure");
+
+// // Setup Restify Server
+// var server = restify.createServer();
+// server.listen(process.env.port || process.env.PORT || 3978, function () {
+//    console.log('%s listening to %s', server.name, server.url); 
+// });
+  
+// // Create chat connector for communicating with the Bot Framework Service
+// var connector = new builder.ChatConnector({
+//     appId: process.env.MicrosoftAppId,
+//     appPassword: process.env.MicrosoftAppPassword,
+//     openIdMetadata: process.env.BotOpenIdMetadata 
+// });
+
+// // Listen for messages from users 
+// server.post('/api/messages', connector.listen());
+
+// /*----------------------------------------------------------------------------------------
+// * Bot Storage: This is a great spot to register the private state storage for your bot. 
+// * We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+// * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+// * ---------------------------------------------------------------------------------------- */
+
+// var tableName = 'botdata';
+// var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+// var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+
+// // Create your bot with a function to receive messages from the user
+// var bot = new builder.UniversalBot(connector);
+// bot.set('storage', tableStorage);
+
+// // Make sure you add code to validate these fields
+// var luisAppId = process.env.LuisAppId;
+// var luisAPIKey = process.env.LuisAPIKey;
+// var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
+
+// const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+
+// // Main dialog with LUIS
+// var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+// var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+// .matches('Greeting', (session) => {
+//     session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
+// })
+// .matches('Help', (session) => {
+//     session.send('You reached Help intent, you said \'%s\'.', session.message.text);
+// })
+// .matches('Cancel', (session) => {
+//     session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
+// })
+// /*
+// .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
+// */
+// .onDefault((session) => {
+//     session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+// });
+
+// bot.dialog('/', intents);    
+
